01c0eeef7bdb2015e4e59037fdf203ceef15e7dc
Murderzy <anntarasiuk@gmail.com>
2022-09-26 14:47:27 +0300

    add work with files

--- src/learn/Main.java
+++ src/learn/Main.java
@@ -1,13 +1,15 @@
 package learn;
 
 import learn.OOP.Library;
+import learn.files.FilesDemo;
 
 public class Main {
     public static void main(String[] args) {
         //new DataTypes().Run();
         //new Complex().Run();
-        new Library().Run();
+        //new Library().Run();
         //new SerializationDemo().Run();
         //new LibrarySerialize().dataRead();
+        new FilesDemo().run();
     }
 }
